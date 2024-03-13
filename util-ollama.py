6b799affba4d42f273f5441d061ca312dbd48f6f
Sam Thomas <samthomas@aiml.team>
2024-03-13 18:19:58 +1030

    utility storing the gpu utulisation in csv

--- util-ollama.py
+++ util-ollama.py
@@ -1,11 +1,35 @@
-from datasets import load_dataset
-import pandas as pd
-ds = load_dataset("nvidia/HelpSteer")
-
-train = pd.DataFrame(ds['train']) # len(train) = 35331 (95%)
-val = ds['validation']
-
-print(train.head())
+import os
+import subprocess
+import time
+import ollama 
+import datetime
 
 
 
+def chat_with_bot(question):
+    response = ollama.chat(model='mistral', messages=[
+      {
+        'role': 'user',
+        'content': question,
+        'stream': False
+      },
+    ])
+    return response
+
+def record_resource_utilisation(question):
+    os.system('!rm -f log_compute.csv')
+    logger_fname = 'log_compute.csv'
+    logger_pid = subprocess.Popen(
+      ['python3', 'log_gpu_cpu_stats.py',
+      logger_fname,
+      '--loop',  '0.2',  # Interval between measurements, in seconds (optional, default=1)
+      ])
+    print('Started logging compute utilisation')
+    response=chat_with_bot()
+    logger_pid.terminate()
+
+    print(response['message']['content'])
+    
+if __name__ == "__main__":
+    
+    record_resource_utilisation()
